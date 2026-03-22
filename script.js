cbad110029a53b52055bdf7598ec7ba1fefc4e93
Vijay Solomon <32757343+nimblevictories@users.noreply.github.com>
2026-03-22 02:33:08 +0000

    drop pin ability, unlimited.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const map = L.map('map').setView([1.3521, 103.8198], 11);
+const map = L.map("map").setView([1.3521, 103.8198], 11);
 
-L.tileLayer('https://www.onemap.gov.sg/maps/tiles/Default/{z}/{x}/{y}.png', {
-  attribution: '© OneMap',
+L.tileLayer("https://www.onemap.gov.sg/maps/tiles/Default/{z}/{x}/{y}.png", {
+  attribution: "&copy; OneMap",
 }).addTo(map);
 
-map.on('click', function(e) {
+map.on("click", function (e) {
   L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
 });
